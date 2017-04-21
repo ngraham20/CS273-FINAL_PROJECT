87d7712ba7c1bbf37ff981904189f5b7c8b2e47d
christianwilliams20 <christianwilliams20@LAB-SCI-304-10>
2017-04-21 13:16:13 -0700

    making sure this uploaded correctly

--- FInal Project/FInal Project/Main.cpp
+++ FInal Project/FInal Project/Main.cpp
@@ -1,8 +1,9 @@
 //This is the main class that will be incharge of the UI
 //It will allow the user to input 1)average hourly arrival time of new pataints 
 // 2)number of doctors in the ER
-//3) the number of nurses in the ER
+// 3) the number of nurses in the ER
 // It will output the average visit time
 //WIll then allow the user to display all names of patients
 // and also allow acces to a patients info based on his name
 //ask the user if he wants to 1) close 2)rerun with the same numbers 3) rerun with new numbers
+
