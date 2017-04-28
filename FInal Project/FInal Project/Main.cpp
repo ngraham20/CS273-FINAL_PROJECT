70f208d297c5571ca550bb68662d51f45394a292
Blackhat-PC\Blackhat <christianwilliams20@my.whitworth.edu>
2017-04-27 23:39:10 -0700

    working on the UI still

--- FInal Project/FInal Project/Main.cpp
+++ FInal Project/FInal Project/Main.cpp
@@ -7,3 +7,40 @@
 // and also allow acces to a patients info based on his name
 //ask the user if he wants to 1) close 2)rerun with the same numbers 3) rerun with new numbers
 
+#include <string>;
+#include <iostream>;
+
+using namespace std;
+
+int main() {
+	bool run = true;
+	while (run) {
+		int arrival_rate;
+		int total_docs;
+		int total_nurses;
+		double average;
+
+		cout << "Welcome to our Final Project" << endl;
+		cout << "Brought to you by the minds of Christian, Nathanial, and Jude" << endl;
+		cout << "Welcome to Sacred Heart's ER room" << endl;
+		cout << "What is the hourly arrival time of new patients?" << endl;
+		cin >> arrival_rate;
+		cout << "How many doctors are in?" << endl;
+		cin >> total_docs;
+		cout << "How many nurses are in" << endl;
+		cin >> total_nurses;
+		cout << "Ok running the simulation for one week" << endl;
+
+
+
+		cout << "Thank you for enduring this" << endl;
+		cout << "Enter 1 to close" << endl;
+		int close_response = 0;
+		cin >> close_response;
+		if (close_response == 1) {
+			run = false;
+		}
+	}
+
+}
+
